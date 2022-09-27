152a5c708d23ee3fa252bd574830b786457bd84e
Ezislav <edvarganov@gmail.com>
2022-09-27 20:50:04 +0300

    Online-Store done

---
+++ src/main/java/com/example/onlinestore/model/Image.java
@@ -0,0 +1,38 @@
+package com.example.onlinestore.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "images")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "originaFileName")
+    private String originalFileName;
+    @Column(name = "size")
+    private Long size;
+    @Column(name = "contentType")
+    private String contentType;
+    @Column(name = "isPreviewImage")
+    private Boolean isPreviewImage;
+    @Lob
+    private byte[] bytes;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    @JoinColumn()
+    private Product product;
+
+
+}
